bacd10bc242cfe197e1d7334fd19e20a84786aba
Eisntein Ayres <ayresasouza@gmail.com>
2023-07-18 10:21:18 -0300

    final do curso kotlin orientação a objetos

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -5,64 +5,70 @@ fun main() {
 
     println("Bem vindo ao Bytebank!")
 
-    val contaAlex = Conta()
-    contaAlex.titular = "Alex"
-    contaAlex.numero = 1001
-    contaAlex.saldo = 200.0
+    val contaAlex = Conta("Alex", 1001)
+//    contaAlex.titular = "Alex"
+//    contaAlex.numero = 1001
+    contaAlex.deposita(200.0)
+
+    val contaFran = Conta(numero = 1002, titular = "Fran")
+//    contaFran.titular = "Fran"
+//    contaFran.numero = 1002
+    contaFran.deposita(300.0)
 
 
-    val contaFran = Conta()
-    contaFran.titular = "Fran"
-    contaFran.numero = 1002
-    contaFran.saldo = 300.0
+    println("transfer conta fran p alex")
 
-
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.saldo)
-
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.saldo)
-
-
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
-
-    println("numeroX $numeroX")
-    println("numeroY $numeroY")
-
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
-
-    val contaMaria = contaJoao
-    contaMaria.titular = "Maria"
-
-    println(contaJoao.titular)
-
-    contaAlex.saldo += 50.0
-
-
-
-
-
+    if (contaFran.transfere(800.0, contaAlex)){
+        println("tranf sucedida")
+    }else{
+        println("falha")
+    }
 }
 
+class Conta (
+    var titular: String,
+    val numero: Int
+) {
 
-
-
-class Conta {
-
-    var titular = ""
-    var numero = 0
+//    var titular = titular
+//    var numero = numero
     var saldo = 0.0
+        private set
 
     var cpf = ""
     var nomePai =""
     var nomeMae = ""
-}
 
-fun deposita(conta:Conta, valor:Double){
-    conta.saldo += valor
+//    constructor(titular: String, numero: Int){
+//        this.titular = titular
+//        this.numero = numero
+//    }
+
+    fun deposita( valor:Double){
+        if (valor > 0) {
+            this.saldo += valor
+        }
+    }
+
+    fun saca(valor: Double){
+
+        if(this.saldo >= valor){
+            saldo -= valor
+//            return error(message = "Saldo insuficiente")
+//        }else {
+//            println("Valor sacado com sucesso")
+        }
+
+    }
+
+    fun transfere(valor: Double, contaDestino: Conta): Boolean {
+
+        if(this.saldo >= valor){
+                saldo -= valor
+                contaDestino.deposita(valor)
+                    return true
+                    }
+                return false
+        }
+
 }
